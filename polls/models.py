7b003f33126012afee2df4163d78f895cf1d7542
SK-CSE <saurabh.cse04@gmail.com>
2016-05-08 11:54:18 +0530

    second commit

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+#test
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
